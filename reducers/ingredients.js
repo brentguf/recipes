8c94144da82c4de3fd54b5fe67dbaeea9161b554
Brent Guffens <brentguf@users.noreply.github.com>
2018-02-01 17:15:43 +0100

    constants in reducers

--- reducers/ingredients.js
+++ reducers/ingredients.js
@@ -1,6 +1,8 @@
+import { ADD_INGREDIENT } from '../constants/action-types';
+
 const ingredientsReducer = (ingredients = [], action) => {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       const newIngredient = {
         recipe: action.recipe,
         name: action.name,
