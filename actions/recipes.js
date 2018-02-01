702f5309ebf8cd041eca4b4373286561f0bc4eb9
Brent Guffens <brentguf@users.noreply.github.com>
2018-02-01 17:12:27 +0100

    action types

--- actions/recipes.js
+++ actions/recipes.js
@@ -1,5 +1,7 @@
+import { ADD_RECIPE } from '../constants/action-types'
+
 const addRecipe = (name) => (
-  { type: 'ADD_RECIPE', name: name }
+  { type: ADD_RECIPE, name: name }
 );
 
 export default addRecipe;
