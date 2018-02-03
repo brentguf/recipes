210e39ed2a54d1b21d397827e9e297602ffe9c25
Brent Guffens <brentguf@users.noreply.github.com>
2018-02-03 18:42:08 +0100

    recipes action/reducer

--- actions/recipes.js
+++ actions/recipes.js
@@ -4,4 +4,8 @@ const addRecipe = (name) => (
   { type: ADD_RECIPE, name: name }
 );
 
+const setRecipes = (recipes) => {
+  { type: SET_RECIPES, recipes: recipes }
+}
+
 export default addRecipe;
--- reducers/recipes.js
+++ reducers/recipes.js
@@ -1,9 +1,11 @@
-import { ADD_RECIPE } from '../constants/action-types';
+import { ADD_RECIPE, SET_RECIPES } from '../constants/action-types';
 
 const recipesReducer = (recipes = [], action) => {
   switch (action.type) {
     case ADD_RECIPE:
       return recipes.concat({ name: action.name });
+    case SET_RECIPES:
+      return recipes;
     default:
       return recipes;
   }
